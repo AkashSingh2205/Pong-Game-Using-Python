d82145e90adb522dba2c8871e573c426dbacb350
AKDOMAIN <107293308+AkashSingh2205@users.noreply.github.com>
2023-01-24 15:35:57 +0530

    Add files via upload

---
+++ Pong.py
@@ -0,0 +1,142 @@
+import turtle
+import winsound
+
+
+win = turtle.Screen()
+win.title("Pong by Akash")
+win.bgcolor("black")
+win.setup(width=800, height=600)
+win.tracer(0)
+
+
+#Score
+score_a = 0
+score_b = 0
+
+#Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("blue")
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350,0)
+
+
+
+#Paddle B
+
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("blue")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350,0)
+
+#Ball
+
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("blue")
+ball.penup()
+ball.goto(0,0)
+ball.dx = 0.5
+ball.dy = -0.5
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("White")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A: 0 Player B: 0", align="center", font=("Courier", 24, "normal"))
+
+
+
+#Functions
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
+
+#Keyboard binding
+win.listen()
+win.onkeypress(paddle_a_up, "w")
+win.onkeypress(paddle_a_down, "s")
+win.onkeypress(paddle_b_up, "Up")
+win.onkeypress(paddle_b_down, "Down")
+
+
+
+
+# Main game loop
+while True:
+    win.update()
+
+    #Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    #Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        winsound.PlaySound("F:\\PY\\free-sound-effects-BOUNCED_.mp3", winsound.SND_ASYNC)
+    
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        winsound.PlaySound("F:\\PY\\free-sound-effects-BOUNCED_.mp3", winsound.SND_ASYNC)
+    
+    if ball.xcor() > 390:
+        ball.goto(0,0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+    
+
+    
+    #paddle and ball collisions
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -50):
+        ball.setx(340)
+        ball.dx *= -1
+        winsound.PlaySound("F:\\PY\\free-sound-effects-BOUNCED_.mp3", winsound.SND_ASYNC)
+
+
+    if (ball.xcor() < -340 and ball.xcor() < -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -50):
+        ball.setx(-340)
+        ball.dx *= -1
+        winsound.PlaySound("F:\\PY\\free-sound-effects-BOUNCED_.mp3", winsound.SND_ASYNC)
+        
+
+
+
